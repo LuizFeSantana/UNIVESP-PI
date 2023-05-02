a5013f95ad73a694163027f990a7b3ab425c326a
LuizFeSantana <60863484+LuizFeSantana@users.noreply.github.com>
2023-05-01 21:35:18 -0300

    mudança

--- Pagina_Web/projeto_cad_animal/projeto_cad_animal/settings.py
+++ Pagina_Web/projeto_cad_animal/projeto_cad_animal/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'projeto_cad_animal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
