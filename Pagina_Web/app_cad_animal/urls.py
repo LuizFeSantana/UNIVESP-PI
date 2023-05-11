79621658c3bd7bf34934dd9de5094cbe3ea21865
LuizFeSantana <60863484+LuizFeSantana@users.noreply.github.com>
2023-05-10 23:23:34 -0300

    paginas

--- Pagina_Web/app_cad_animal/urls.py
+++ Pagina_Web/app_cad_animal/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import home,sobre
+from .views import home,sobre, cad_animal, cadastro, tutor
 
 urlpatterns = [
     path('', home),
     path('sobre', sobre),
+    path('cad_animal', cad_animal),
+    path('cadastro', cadastro),
+    path('tutor', tutor),
 ]
--- Pagina_Web/app_cad_animal/views.py
+++ Pagina_Web/app_cad_animal/views.py
@@ -6,3 +6,12 @@ def home(request):
 
 def sobre(request):
     return render(request,'sobre.html')
+
+def cad_animal(request):
+    return render(request,'cad_animal.html')
+
+def tutor(request):
+    return render(request,'tutor.html')
+
+def cadastro(request):
+    return render(request,'cadastro.html')
