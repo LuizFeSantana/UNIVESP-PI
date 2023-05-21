e95ecb6f75126937ba4888c898e47d3343f0e7cd
LuizFeSantana <60863484+LuizFeSantana@users.noreply.github.com>
2023-05-21 12:54:31 -0300

    UpdateView

--- Tutor_Amigo/cadastros/urls.py
+++ Tutor_Amigo/cadastros/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 #importa as views criadas
 from .views import UsuarioCreate, OngsCreate, ClinicaCreate, VeterinarioCreate
+from .views import UsuarioUpdate, OngsUpdate, ClinicaUpdate, VeterinarioUpdate
 
 urlpatterns = [
     #path('endereço/', MinhasView.as_view(), name= 'nome da url')
@@ -11,4 +12,8 @@ urlpatterns = [
     path('cadastros/clinica', ClinicaCreate.as_view(), name= 'cadastro-clinica'),
     path('cadastros/veterinario', VeterinarioCreate.as_view(), name= 'cadastro-veterinario'),
     
+    path('editar/tutor/<int:pk>/', UsuarioUpdate.as_view(), name= 'editar-tutor'),
+    path('editar/ong/<int:pk>/', OngsUpdate.as_view(), name= 'editar-ong'),
+    path('editar/clinica/<int:pk>/', ClinicaUpdate.as_view(), name= 'editar-clinica'),
+    path('editar/veterinario/<int:pk>/', VeterinarioUpdate.as_view(), name= 'editar-veterinario'),
 ]
--- Tutor_Amigo/cadastros/views.py
+++ Tutor_Amigo/cadastros/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from .models import Animais, Usuario, Ongs, Servicos, Veterinario, Clinica, Localizacao, LarDefinitivo, LarTemporario
 
@@ -25,6 +25,33 @@ class ClinicaCreate(CreateView):
     success_url = reverse_lazy('index')
 
 class VeterinarioCreate(CreateView):
+    model = Veterinario
+    fields = ['Nome', 'CRMV', 'celular', 'endereco']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+
+######################## UPDATE ########################
+
+class UsuarioUpdate(UpdateView):
+    model = Usuario
+    fields = ['Nome', 'cpf', 'rg', 'Funcao', 'email', 'endereco']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+class OngsUpdate(UpdateView):
+    model = Ongs
+    fields = ['CNPJ', 'RazaoSocial', 'email', 'resp', 'telefone', 'endereco', 'cep']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+class ClinicaUpdate(UpdateView):
+    model = Clinica
+    fields = ['CNPJ', 'RazaoSocial', 'medicoResp', 'endereco', 'bairro', 'estado', 'cep', 'telefone']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+class VeterinarioUpdate(UpdateView):
     model = Veterinario
     fields = ['Nome', 'CRMV', 'celular', 'endereco']
     template_name = 'cadastros/form.html'
